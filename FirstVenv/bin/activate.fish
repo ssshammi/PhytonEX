986bfe06b5f22826f4ff4c8110edb60639c9c9a5
shammi <ssshammi@hotmail.com>
2021-02-28 00:47:16 +0000

    django default application
    django default application

--- FirstVenv/bin/activate.fish
+++ FirstVenv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/shammiseth/FirstVenv"
+set -gx VIRTUAL_ENV "/Users/shammiseth/PhytonEx/PhytonEX/FirstVenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
